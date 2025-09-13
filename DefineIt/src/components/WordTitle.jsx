66bc761c09b473994007568bfb7592e7dd5315e9
Karl <karlbautista234@gmail.com>
2025-09-14 00:58:15 +0800

    added the volume of the words

--- DefineIt/src/components/WordTitle.jsx
+++ DefineIt/src/components/WordTitle.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useWord from './WordZustand'
 import Volume from "../assets/medium-volume.png"
-const WordTitle = ({ word}) => {
+const WordTitle = ({ word }) => {
+    const [ audioUrls, setAudioUrls ] = useState([]);
+
+    useEffect(() => {
+     const audioUrl = word[0].phonetics.filter((phonetic) => phonetic.audio !== "");
+     setAudioUrls(audioUrl);
+     console.log("ito mga ", audioUrl)
+    }, [word]);
+
+  const playVolume = () => {
+      const audioSrc = audioUrls[0]?.audio;
+    if(audioSrc && audioSrc.trim() !== ""){
+     const audio = new Audio(audioUrls[0].audio);
+     audio.play();
+    }
+    else{
+        alert("sorry, no available sound")
+    }
+  }
 
   return (
     <div className='w-full h-32'>
@@ -10,7 +28,8 @@ const WordTitle = ({ word}) => {
                     <h2 className='text-4xl font-bold'>{word[0].word}</h2>
                     <div className='text-[#6b7280] text-lg'>{word[0].phonetic}
                         <span className='w-[50px] h-[50px]'>
-                            <img src={Volume} alt="Volume Icon" className='inline w-[25px] h-[25px] ml-2 cursor-pointer' />
+                            <img src={Volume} alt="Volume Icon" className='inline w-[25px] h-[25px] ml-2 cursor-pointer' 
+                            onClick={() => playVolume()}/>
                         </span>
                     </div>
                     <div className='text-[#6b7280] italic'>
